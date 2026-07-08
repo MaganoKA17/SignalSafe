476f21c5f57b3d2ce3179dbdf92713edf47347d4
Kgosi-E-tsile Magano <kgmagjhb025@student.wethinkcode.co.za>
2026-07-08 11:02:32 +0200

    refactor: added button on the app portion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/NavBar';
 import './App.css';
 
 function App(){
+  const [started, setStarted] = useState(false);
   return (
     <div className='app'>
       <Navbar/>
+      {!started && <Home onGetStarted={() => setStarted(true)}/>}
     </div>
-  )
+  );
 
 }
 export default App;
