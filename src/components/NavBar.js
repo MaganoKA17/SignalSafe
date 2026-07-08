1efd55598e7b53269d767006491f53b05c9d0e1b
Kgosi-E-tsile Magano <kgmagjhb025@student.wethinkcode.co.za>
2026-07-08 07:14:52 +0200

    feat: created method for Navbar file

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import react from 'react';
+import styles from '..styles/Navbar.module.css';
+
+function Navbar(){
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                SignalSafe
+            </div>
+            <div className={styles.tagline}>
+                Detecting exploitation before harm occurs
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
